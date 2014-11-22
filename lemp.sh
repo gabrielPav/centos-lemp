1a85e898a8de0e4abd6c838c6ca5fa2c12caf794
gpav <gabriel.pavell@gmail.com>
2014-11-22 19:44:29 +0200

    Update ngx_pagespeed version to 1.9.32.2

--- lemp.sh
+++ lemp.sh
@@ -54,7 +54,7 @@ sudo yum -y install openssl openssl-devel gcc-c++ pcre-dev pcre-devel zlib-devel
 
 # Download ngx_pagespeed
 cd
-NPS_VERSION=1.9.32.1
+NPS_VERSION=1.9.32.2
 wget https://github.com/pagespeed/ngx_pagespeed/archive/release-${NPS_VERSION}-beta.zip
 unzip release-${NPS_VERSION}-beta.zip
 cd ngx_pagespeed-release-${NPS_VERSION}-beta/
